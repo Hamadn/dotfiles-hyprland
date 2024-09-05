634c52d12422bbfe7aef2b36799fea53cc5900ba
hamad <nhamad919@gmail.com>
2024-09-05 06:24:45 +0400

    removed the bufferline for minimalism and changed colorscheme

--- .config/nvim/lua/void/core/options.lua
+++ .config/nvim/lua/void/core/options.lua
@@ -3,18 +3,19 @@ vim.cmd("let g:netrw_liststyle = 3")
 local opt = vim.opt -- for conciseness
 
 -- line numbers
-opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
-
+--opt.relativenumber = true -- show relative line numbers
+--opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+vim.opt.fillchars = { eob = " " }
+vim.g.mkdp_browser = "qutebrowser"
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
-
+opt.linebreak = true
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
@@ -28,7 +29,8 @@ opt.cursorline = true -- highlight the current cursor line
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.signcolumn = "no" -- show sign column so that text doesn't shift
+opt.showtabline = 0 -- hide the tabline
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
