44d9527952a2d365b3e17e67b63502a8ce177d0e
hamad <nhamad919@gmail.com>
2024-09-08 07:37:36 +0400

    added my wezterm config and a script to install nerdfonts

---
+++ .wezterm.lua
@@ -0,0 +1,56 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("UbuntuMono Nerd Font Mono")
+config.font_size = 20
+config.window_padding = {
+	left = 7,
+	right = 7,
+	top = 0,
+	bottom = 0,
+}
+
+config.enable_tab_bar = false
+
+config.color_scheme = "Gruvbox Material (Gogh)"
+
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
+
+--config.colors = {
+--	foreground = "#CBE0F0",
+--	background = "#000000",
+--	cursor_bg = "#47FF9C",
+--	cursor_border = "#47FF9C",
+--	cursor_fg = "#011423",
+--	selection_bg = "#033259",
+--	selection_fg = "#CBE0F0",
+--	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277FF", "#24EAF7", "#24EAF7" },
+--	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#A277FF", "#24EAF7", "#24EAF7" },
+--}
+
+config.window_decorations = "NONE"
+
+config.window_background_opacity = 1
+
+return config
